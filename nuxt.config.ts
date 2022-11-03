43ed71047f6612d23605d29e675bc75433761dd5
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-11-03 15:00:24 +0100

    fix: :test_tube: env is not getting read by nitro?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
 	buildModules: ["@nuxtjs/tailwindcss"],
 	runtimeConfig: {
-		MONGO_ADDRESS: process.env.MONGO_ADDRESS
+		NUXT_MONGO_ADDRESS: process.env.NUXT_MONGO_ADDRESS || 'localhost'
 	},
 	nitro: {
 		plugins: ["@/server/database/grievous.ts"]
--- server/database/grievous.ts
+++ server/database/grievous.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose"
 
 export default async (_nitroApp) => {
     const config = useRuntimeConfig()
+
+    console.log(config)
+
     mongoose
-    .connect(`mongodb://${config.MONGO_ADDRESS}:27017/kenobi`)
+    .connect(`mongodb://${config.NUXT_MONGO_ADDRESS}:27017/kenobi`)
     .then(() => {
-        console.log(`Connected to ${config.MONGO_ADDRESS}`)
+        console.log(`Connected to ${config.NUXT_MONGO_ADDRESS}`)
     })
     .catch((e) => {
-        console.log(`Error connecting to ${config.MONGO_ADDRESS}: ${e}`)
+        console.log(`Error connecting to ${config.NUXT_MONGO_ADDRESS}: ${e}`)
     })
 }
