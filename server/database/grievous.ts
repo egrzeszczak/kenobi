282289475a6828040ae07b2bc6541cd52ebdf237
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-11-04 12:27:11 +0100

    fix: :wrench: 1.0.8 switch fron runtime config to process.env for grievous.ts
    Somehow nitro doesn't want to co-operate, does this solution work? idk

--- server/database/grievous.ts
+++ server/database/grievous.ts
@@ -1,16 +1,22 @@
 import mongoose from "mongoose"
 
 export default async (_nitroApp) => {
-    const config = useRuntimeConfig()
+    // const config = useRuntimeConfig()
 
-    console.info(`[INFO] Connecting to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}`)
+    console.log(process.env.NUXT_MONGO_ADDRESS)
+
+    // console.info(`[INFO] Connecting to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}`)
+    console.info(`[INFO] Connecting to GrievousDB ${process.env.NUXT_MONGO_ADDRESS}`)
 
     mongoose
-        .connect(`mongodb://${config.public.NUXT_MONGO_ADDRESS}:27017/kenobi`)
+        // .connect(`mongodb://${config.public.NUXT_MONGO_ADDRESS}:27017/kenobi`)
+        .connect(`mongodb://${process.env.NUXT_MONGO_ADDRESS}:27017/kenobi`)
         .then(() => {
-            console.info(`[INFO] Connected to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}`)
+            // console.info(`[INFO] Connected to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}`)
+            console.info(`[INFO] Connected to GrievousDB ${process.env.NUXT_MONGO_ADDRESS}`)
         })
         .catch((e) => {
-            console.error(`[ERROR] Error connecting to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}: ${e}`)
+            // console.error(`[ERROR] Error connecting to GrievousDB ${config.public.NUXT_MONGO_ADDRESS}: ${e}`)
+            console.error(`[ERROR] Error connecting to GrievousDB ${process.env.NUXT_MONGO_ADDRESS}: ${e}`)
         })
 }
